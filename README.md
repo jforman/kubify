478dbc38944ed7f0a66029ffbdd43008b800c799
jeffrey forman <jeff@jeffreyforman.net>
2018-01-02 17:16:01 -0500

    add dryrun support, debug logging, as well as documentation. created a simple string replace template dict for standardizing path replacements for binary runs

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-### Installing Kubernetes, the hard way.
+# Installing Kubernetes, the hard way.
 
 Based on https://github.com/kelseyhightower/kubernetes-the-hard-way
+
+This script is meant to follow Kelsey Hightower's tutorial of installing Kubernetes. It was born out of the fact that I run CoreOS VM's at home on a Ubuntu (sometimes Debian) libvirt/kvm server, and wish to play around with CoreOS+Kubernetes tech at home. None of the other tutorials I found online spent much time on bare-metal, or in this case VM, installs. Most other scripts expected the Kubernetes cluster to live on one of the popular public clouds (GCE/AWS/Azure), and I wanted to do have my own install on my own terms.
+
+Why not the public cloud? Mostly because of cost. I've already got a beefy VM server at home. Why not use it?
+
+## Script Usage
+
+usage: kubify.py [-h] [--dry_run] [--kube_ver KUBE_VER] --output_dir
+                 OUTPUT_DIR [--clear_output_dir]
+
+Install Kubernetes, the hard way.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --dry_run             dont actually do anything. (default: False)
+  --kube_ver KUBE_VER   kubernetes version (default: 1.9.0)
+  --output_dir OUTPUT_DIR
+                        base directory where generated configs will be stored.
+                        (default: None)
+  --clear_output_dir    if true, clear the output directory before generating
+                        configs (default: False)
+
+
+### Shortcuts in the script
+
+Given the cfssl and cfssljson commands use a lot of the same directory paths and file references, I've declared the following special variables which will be replaced accordingly when those commands are run.
+
+{CHECKOUT_DIR}: Git checkout directory
+{OUTPUT_DIR}: Directory were certificates and other generated data will be stored.
+
+{ADMIN_DIR}: Directory under OUTPUT_DIR where administrative certificates are stored.
+{BIN_DIR}: Directory under OUTPUT_DIR where binaries related to configuration generation will live.
+{CA_DIR}: Directory under OUTPUT_DIR where certificate authority certificates are stored.
+{TEMPLATE_DIR}: Directory under Git checkout where JSON and other configuration templates lives.
+{TMP_DIR}: Directory under OUTPUT_DIR where intermediate output is stored that can be later consumed by other processes.
--- kubify.py
+++ kubify.py
@@ -4,6 +4,7 @@ import argparse
 import logging
 import os
 import shutil
+import string
 import subprocess
 import sys
 import urllib
@@ -13,128 +14,183 @@ class KubeBuild:
     def __init__(self, cli_args):
         self.args = cli_args
         self.checkout_path = os.path.dirname(os.path.realpath(sys.argv[0]))
-        self.bin_path = os.path.join(self.checkout_path, 'bin')
-        logging.info('Checkout Path: %s, Bin Path: %s.',
-                      self.checkout_path, self.bin_path)
+        logging.info('Checkout Path: %s, Output Dir: %s',
+                      self.checkout_path, self.args.output_dir)
 
-    def get_bin_dir(self):
-        return os.path.join(self.args.output_dir, 'bin')
+
+    def translate_path(self, path):
+        """given string containing special macro, return command line with
+        directories substituted in string."""
+
+        # first define the base directories
+        path_dict = {
+            '{CHECKOUT_DIR}': os.path.dirname(os.path.realpath(sys.argv[0])),
+            '{OUTPUT_DIR}': self.args.output_dir,
+        }
+
+        # now we can update the dict path based upon the base ones above
+        path_dict.update({
+            '{ADMIN_DIR}': os.path.join(path_dict['{OUTPUT_DIR}'], 'admin'),
+            '{BIN_DIR}': os.path.join(path_dict['{OUTPUT_DIR}'], 'bin'),
+            '{CA_DIR}': os.path.join(path_dict['{OUTPUT_DIR}'], 'ca'),
+            '{TEMPLATE_DIR}': os.path.join(path_dict['{CHECKOUT_DIR}'],
+                                           'templates'),
+            '{TMP_DIR}': os.path.join(path_dict['{OUTPUT_DIR}'], 'tmp')
+
+        })
+        logging.debug('translating %s.', path)
+        for key, value in path_dict.iteritems():
+            path = string.replace(path, key, value)
+        logging.debug('returning translated path %s.', path)
+        return path
+
 
     def build(self):
+        """main build sequencer function."""
         self.create_output_dirs()
         self.download_tools()
         self.create_ca_cert_private_key()
         self.create_admin_client_cert()
 
+
     def run_bin_command(self, cmd, return_output=False,
                         cmd_stdin=None,  write_output=''):
         """given a command, run it from the bin directory of the checkout."""
-        command_list = cmd.split()
+        command_list = self.translate_path(cmd).split()
         command_list[0] = os.path.join(self.args.output_dir,
                                        'bin',
-                                       command_list[0])
-        logging.info("command about to run: %s" % command_list)
-        try:
-            output = subprocess.check_output(
-                command_list,
-                stdin=cmd_stdin,
-                )
-        except subprocess.CalledProcessError as err:
-            logging.fatal("Error in running %s. Error: %s", cmd, err)
-            sys.exit(1)
-
-        logging.info("command output: %s", output)
+                                       self.translate_path(command_list[0]))
+        if self.args.dry_run:
+            logging.info('DRYRUN: would have run %s.', command_list)
+        else:
+            try:
+                logging.debug('running %s', command_list)
+                output = subprocess.check_output(
+                    command_list,
+                    stdin=cmd_stdin,
+                    )
+                logging.debug("command output: %s", output)
+            except subprocess.CalledProcessError as err:
+                logging.fatal("Error in running %s. Error: %s", cmd, err)
+                sys.exit(1)
 
         if write_output:
-            out_file = os.path.join(self.args.output_dir, write_output)
-            logging.info("writing output to %s.", out_file)
+            out_file = self.translate_path(write_output)
+
+            if self.args.dry_run:
+                logging.debug('DRYRUN: writing output to %s.', out_file)
+                return
+
+            logging.debug("writing output to %s.", out_file)
             with open(out_file, 'w') as of:
                 of.write(output)
-            logging.info("done writing output to %s.", out_file)
+            logging.debug("done writing output to %s.", out_file)
 
         if return_output:
             return output
 
 
     def create_output_dirs(self):
-        subdirs = ['admin', 'bin', 'ca', 'tmp']
+        subdirs = ['admin', 'bin', 'ca', 'tmp', 'workers']
 
-        if not self.args.clear_output_dir and os.path.exists(self.args.output_dir):
-            logging.fatal('output directory already exists, but you chose not to clear it out '
-                          'first. are old configs still present that you still want to save?')
+        if (
+            not self.args.clear_output_dir and
+            os.path.exists(self.args.output_dir) and
+            not self.args.dry_run
+            ):
+            logging.fatal('output directory already exists, but you chose not '
+                          'to clear it out first. are old configs still '
+                          'present that you still want to save?')
             sys.exit(1)
 
         if os.path.exists(self.args.output_dir):
-            shutil.rmtree(self.args.output_dir)
+            if self.args.dry_run:
+                logging.debug('DRYRUN: would have deleted %s.',
+                             self.args.output_dir)
+            else:
+                shutil.rmtree(self.args.output_dir)
 
-        os.makedirs(self.args.output_dir)
+        if self.args.dry_run:
+            logging.debug('DRYRUN: would have created %s.',
+                          self.args.output_dir)
+        else:
+            os.makedirs(self.args.output_dir)
+
         for current_dir in subdirs:
-            os.makedirs(os.path.join(self.args.output_dir,
-                                     current_dir))
+            if self.args.dry_run:
+                logging.debug('DRYRUN: create directory %s.',
+                              os.path.join(self.args.output_dir,
+                                           current_dir))
+            else:
+                os.makedirs(os.path.join(self.args.output_dir,
+                                         current_dir))
 
     def download_tools(self):
+        files_to_get = {
+            'https://pkg.cfssl.org/R1.2/cfssl_linux-amd64':
+            self.translate_path('{BIN_DIR}/cfssl'),
+            'https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64':
+            self.translate_path('{BIN_DIR}/cfssljson'),
+            'https://storage.googleapis.com/kubernetes-release/release/v%s/bin/linux/amd64/kubectl' % self.args.kube_ver:
+            self.translate_path('{BIN_DIR}/kubectl'),
+        }
+
         logging.info("downloading new set of binary tools")
-        urllib.urlretrieve('https://pkg.cfssl.org/R1.2/cfssl_linux-amd64',
-                           os.path.join(self.get_bin_dir(), 'cfssl'))
-        os.chmod(os.path.join(self.get_bin_dir(), 'cfssl'), 0775)
+        for remotef, localf in files_to_get.iteritems():
+            localf = self.translate_path(localf)
+            if self.args.dry_run:
+                logging.info("DRY RUN: would have downloaded %s to %s.",
+                             remotef, localf)
+                continue
 
-        urllib.urlretrieve('https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64',
-                           os.path.join(self.get_bin_dir(), 'cfssljson'))
-        os.chmod(os.path.join(self.get_bin_dir(), 'cfssljson'), 0775)
+            logging.debug('downloading %s to %s.', remotef, localf)
+            urllib.urlretrieve(remotef, localf)
+            os.chmod(localf, 0775)
 
-        urllib.urlretrieve(
-            'https://storage.googleapis.com/kubernetes-release/release/v%s/bin/linux/amd64/kubectl' % self.args.kube_ver,
-            os.path.join(self.get_bin_dir(), 'kubectl'))
-        os.chmod(os.path.join(self.get_bin_dir(), 'kubectl'), 0775)
         logging.info("done downloading tools")
 
     def create_ca_cert_private_key(self):
         """create ca cert and private key."""
         self.run_bin_command(
-            cmd="cfssl gencert -initca %s/ca-csr.json" % (
-                os.path.join(self.checkout_path,'templates')),
-            write_output='tmp/cfssl_initca.output')
-
+            cmd="cfssl gencert -initca {TEMPLATE_DIR}/ca-csr.json",
+            write_output='{TMP_DIR}/cfssl_initca.output')
         self.run_bin_command(
-            cmd='cfssljson -bare -f %s %s/ca' % (
-                os.path.join(self.args.output_dir, 'tmp',
-                             'cfssl_initca.output'),
-                os.path.join(self.args.output_dir, 'ca')
-                )
-            )
+            cmd='cfssljson -bare -f {TMP_DIR}/cfssl_initca.output {CA_DIR}/ca')
+
 
     def create_admin_client_cert(self):
         """create admin client certificate"""
         self.run_bin_command(
-            cmd=("cfssl gencert -ca=%(output_dir)s/ca/ca.pem "
-                 "-ca-key=%(output_dir)s/ca/ca-key.pem "
-                 "-config=%(template_dir)s/ca-config.json "
-                 "-profile=kubernetes %(template_dir)s/admin-csr.json" %
-                 {'template_dir': os.path.join(self.checkout_path,
-                                               'templates'),
-                  'output_dir': self.args.output_dir}
-                 ),
-            write_output='tmp/cfssl_gencert_admin.output')
+            cmd=("cfssl gencert -ca={OUTPUT_DIR}/ca/ca.pem "
+                 "-ca-key={OUTPUT_DIR}/ca/ca-key.pem "
+                 "-config={TEMPLATE_DIR}/ca-config.json "
+                 "-profile=kubernetes {TEMPLATE_DIR}/admin-csr.json"),
+            write_output='{TMP_DIR}/cfssl_gencert_admin.output')
 
         self.run_bin_command(
-            cmd='cfssljson -bare -f %s %s/admin' % (
-                os.path.join(self.args.output_dir,
-                             'tmp', 'cfssl_gencert_admin.output'),
-                os.path.join(self.args.output_dir, 'admin')
-                ),
+            cmd=('cfssljson -bare -f {TMP_DIR}/cfssl_gencert_admin.output '
+                 '{ADMIN_DIR}/admin')
             )
 
 
 def main():
+    """main for Kubify script."""
     parser = argparse.ArgumentParser(
         description='Install Kubernetes, the hard way.',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--dry_run',
+                        action='store_true',
+                        help='dont actually do anything.')
+    parser.add_argument('--debug',
+                        action='store_true',
+                        help='enable debug-level logging.')
     parser.add_argument('--kube_ver',
                         dest='kube_ver',
                         help='kubernetes version',
                         default='1.9.0')
     parser.add_argument('--output_dir',
                         dest='output_dir',
+                        required=True,
                         help=('base directory where generated configs '
                               'will be stored.'))
     parser.add_argument('--clear_output_dir',
@@ -143,9 +199,14 @@ def main():
                         help='if true, clear the output directory before generating configs')
 
     args = parser.parse_args()
+
+    if args.debug:
+        log_level=logging.DEBUG
+    else:
+        log_level=logging.INFO
     logging.basicConfig(
-        format='%(asctime)-15s %(message)s',
-        level=logging.INFO)
+        format='%(asctime)-10s %(filename)s:%(lineno)d %(levelname)s %(message)s',
+        level=log_level)
     k8s = KubeBuild(args)
     k8s.build()
 
