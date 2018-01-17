a1487bc0d29fd2b60b9328f79c5cb480e032e031
jeffrey forman <jeff@jeffreyforman.net>
2018-01-16 20:18:35 -0500

    Add support to bootstrap kubernetes on the control plane (controller machines)
    * added support to ignore errors when running commands via ignore_errors flag,
    which is piped to commands which need it. this was necessary when trying to stop
    a brand new etcd-member service which might not return with a 0 exit code
    if it's already broken
    * adds cluster_cidr and service_cidr to kubify.conf which specifies various networks
    in use by kubernetes
    * adds templates for kube-apiserver, kube-controller-manager and kube-scheduler

--- kubify.py
+++ kubify.py
@@ -100,8 +100,10 @@ class KubeBuild:
         self.create_encryption_configs()
         self.deploy_encryption_configs()
         self.bootstrap_etcd_cluster('controller')
+        self.bootstrap_control_plane()
 
-    def scp_file(self, local_path, remote_user, remote_host, remote_path):
+    def scp_file(self, local_path, remote_user, remote_host, remote_path,
+                 ignore_errors=False):
         """copy the local file to the remote destination."""
         ssh_args = "-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
         self.run_command(
@@ -112,10 +114,12 @@ class KubeBuild:
                 'remote_user': remote_user,
                 'remote_host': remote_host,
                 'remote_path': remote_path
-            }
+            },
+            ignore_errors=ignore_errors,
         )
 
-    def run_command_via_ssh(self, remote_host, remote_user, command):
+    def run_command_via_ssh(self, remote_host, remote_user, command,
+                            ignore_errors=False):
         ssh_args = ('-o UserKnownHostsFile=/dev/null '
                     '-o StrictHostKeyChecking=no '
                     '-t')
@@ -126,7 +130,8 @@ class KubeBuild:
                 'ssh_args': ssh_args,
                 'remote_user': remote_user,
                 'remote_host': remote_host,
-                'command': command }
+                'command': command },
+            ignore_errors=ignore_errors,
             )
 
 
@@ -183,7 +188,7 @@ class KubeBuild:
                 '~/')
 
     def run_command(self, cmd, return_output=False,
-                    cmd_stdin=None,  write_output=''):
+                    cmd_stdin=None,  write_output='', ignore_errors=False):
         """given a command, translate needed paths and run it."""
         command_list = self.translate_path(cmd).split()
 
@@ -200,7 +205,10 @@ class KubeBuild:
             except subprocess.CalledProcessError as err:
                 logging.fatal("Error in running %s. Error: %s", command_list,
                               err)
-                sys.exit(1)
+                if ignore_errors:
+                    logging.info('ERROR IGNORED, continuing on.')
+                else:
+                    sys.exit(1)
 
         if write_output:
             out_file = self.translate_path(write_output)
@@ -283,6 +291,179 @@ class KubeBuild:
             os.chmod(localf, 0775)
 
         logging.info("done downloading tools")
+
+    def bootstrap_control_plane(self):
+        """bootstrap kubernetes components on the controller hosts."""
+        node_type = 'controller'
+        nodes = self.config.get(node_type, 'ip_addresses').split(',')
+        etcd_servers = ",".join(['https://%s:2379' % x for x in nodes])
+        install_dir = self.config.get('general', 'install_dir')
+        kube_bins = ['kube-apiserver', 'kube-controller-manager',
+                     'kube-scheduler', 'kubectl']
+        prefix = self.config.get(node_type, 'prefix')
+        remote_user = self.config.get(node_type, 'remote_user')
+
+        # write systemd service templates
+        template_vars = {'CERTS_DIR': self.config.get('general',
+                                                      'ssl_certs_dir'),
+                         'CLUSTER_NAME': self.config.get(
+                             'general',
+                             'cluster_name'),
+                         'CLUSTER_CIDR': self.config.get('general',
+                                                         'cluster_cidr'),
+                         'ETCD_SERVERS': etcd_servers,
+                         'INSTALL_DIR': self.config.get('general',
+                                                        'install_dir'),
+                         'SERVICE_CIDR': self.config.get('general',
+                                                         'service_cidr'),
+        }
+
+        # write kube-controller-manager
+        rendered_template = helpers.render_template(
+            self.translate_path('{TEMPLATE_DIR}/kube-controller-manager.service'),
+                template_vars
+            )
+        template_out_path = self.translate_path(
+            '{API_SERVER_DIR}/kube-controller-manager.service',
+            )
+        if self.args.dry_run:
+            logging.info('DRYRUN: would have written kube-controller-manager.service '
+                         'to %s.', template_out_path)
+        else:
+            with open(template_out_path, 'w') as tp:
+                tp.write(rendered_template)
+
+        # write kube-scheduler
+        rendered_template = helpers.render_template(
+            self.translate_path('{TEMPLATE_DIR}/kube-scheduler.service'),
+                template_vars
+            )
+        template_out_path = self.translate_path(
+            '{API_SERVER_DIR}/kube-scheduler.service',
+            )
+        if self.args.dry_run:
+            logging.info('DRYRUN: would have written kube-scheduler.service '
+                         'to %s.', template_out_path)
+        else:
+            with open(template_out_path, 'w') as tp:
+                tp.write(rendered_template)
+
+        logging.info('bootstraping kubernetes on %s nodes.', node_type)
+
+        for cur_index in range(0, self.get_node_count(node_type)):
+            hostname = helpers.hostname_with_index(
+                self.config.get('controller', 'prefix'),
+                self.get_node_domain(),
+                cur_index)
+            logging.info('bootstrapping %s for kubernetes.', hostname)
+            self.run_command_via_ssh(
+                nodes[cur_index],
+                remote_user,
+                'sudo mkdir -p %(install_dir)s/bin/' % {
+                    'install_dir': install_dir}
+            )
+
+            self.run_command_via_ssh(
+                nodes[cur_index],
+                remote_user,
+                'sudo systemctl stop kube-scheduler.service',
+                ignore_errors=True)
+
+            self.run_command_via_ssh(
+                nodes[cur_index],
+                remote_user,
+                'sudo systemctl stop kube-controller-manager.service',
+                ignore_errors=True)
+
+            self.run_command_via_ssh(
+                nodes[cur_index],
+                remote_user,
+                'sudo systemctl stop kube-apiserver.service',
+                ignore_errors=True)
+
+            for cur_file in kube_bins:
+                self.scp_file(
+                    '{BIN_DIR}/%s' % cur_file,
+                    remote_user,
+                    nodes[cur_index],
+                    '~')
+                self.run_command_via_ssh(
+                    nodes[cur_index],
+                    remote_user,
+                    'sudo cp %(cur_file)s %(install_dir)s/bin/' % {
+                        'cur_file': cur_file,
+                        'install_dir': install_dir
+                    }
+                )
+                self.run_command_via_ssh(
+                    nodes[cur_index],
+                    remote_user,
+                    'sudo chmod +x %(install_dir)s/bin/%(cur_file)s' % {
+                        'cur_file': cur_file,
+                        'install_dir': install_dir,
+                    }
+                )
+            # write systemd service templates
+            template_vars.update({'IP_ADDRESS': nodes[cur_index]})
+
+            rendered_template = helpers.render_template(
+                self.translate_path('{TEMPLATE_DIR}/kube-apiserver.service'),
+                template_vars
+            )
+            template_path = self.translate_path(
+                '{API_SERVER_DIR}/%s-kube-apiserver.service' % hostname,
+            )
+            if self.args.dry_run:
+                logging.info('DRYRUN: would have written worker csr json '
+                             'template to %s.', template_path)
+            else:
+                with open(template_path, 'w') as tp:
+                    tp.write(rendered_template)
+
+            self.scp_file(
+                template_path,
+                remote_user,
+                nodes[cur_index],
+                '~/kube-apiserver.service'
+                )
+
+            self.scp_file(
+                self.translate_path('{API_SERVER_DIR}/kube-controller-manager.service'),
+                remote_user,
+                nodes[cur_index],
+                '~/',
+                )
+
+            self.scp_file(
+                self.translate_path('{API_SERVER_DIR}/kube-scheduler.service'),
+                remote_user,
+                nodes[cur_index],
+                '~/',
+                )
+
+            self.run_command_via_ssh(
+                nodes[cur_index],
+                remote_user,
+                ('sudo mv kube-apiserver.service kube-scheduler.service '
+                 'kube-controller-manager.service /etc/systemd/system/'))
+
+            self.run_command_via_ssh(
+                nodes[cur_index],
+                remote_user,
+                'sudo systemctl daemon-reload')
+
+            self.run_command_via_ssh(
+                nodes[cur_index],
+                remote_user,
+                ('sudo systemctl enable kube-apiserver kube-controller-manager '
+                'kube-scheduler'))
+
+            self.run_command_via_ssh(
+                nodes[cur_index],
+                remote_user,
+                ('sudo systemctl start kube-apiserver kube-controller-manager '
+                'kube-scheduler'))
+
 
     def create_ca_cert_private_key(self):
         """create ca cert and private key."""
@@ -646,6 +827,7 @@ def main():
 
     k8s = KubeBuild(args)
     k8s.build()
+    logging.info('completed running kubernetes build.')
 
 
 if __name__ == '__main__':
